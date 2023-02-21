9dce6e5d59dd5e5639dc07ffcd9ee7467bace1a6
Aleksa Jevtic <aleksa-jevtic@outlook.com>
2023-02-21 11:54:25 +0100

    Premier commit : ajout de comptes

---
+++ Compte.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class Compte
+    {
+        public static List<int> CompteList = new List<int>();
+        int _ID;
+        double _solde = 0;
+        List<double> _histoTransac = new List<double>();
+
+        public Compte(int ID, double solde = 0) 
+        { 
+            _ID = ID;
+            _solde = solde;
+            CompteList.Add(_ID);
+        
+        
+        }
+
+        public void afficher()
+        {
+            Console.WriteLine($"Affichage du compte {_ID}");
+            Console.WriteLine($"Solde actuel = {_solde} cacahuète(s)\n");
+            Console.WriteLine("Historique des transactions");
+
+            if (!_histoTransac.Any())
+            {
+                Console.WriteLine("Aucune transaction dans l'historique");
+
+            }
+            else
+            {
+                foreach(double montant in _histoTransac)
+                {
+                    Console.WriteLine(montant);
+
+                }
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Compte> Banque = new List<Compte>();
+
+            using (StreamReader file = new StreamReader("Comptes.csv"))
+            {
+
+                string line;
+
+
+                while ((line = file.ReadLine()) != null)
+                {
+
+
+
+
+                    string[] subs = line.Split(';'); //Séparation des éléments du .CSV
+
+                    if (subs.Count() < 1 || subs.Count() > 2) //Si le nb d'elements est deff de 1 et 2, on ne fait rien
+                    {
+                        continue;
+                    }
+
+                    if (int.TryParse(subs[0], out int d)) //si on arrive à convertir le premier élément en int...
+
+                    {
+                        if (d <= 0 || Compte.CompteList.Contains(d)) //Si d est négatif ou égal à 0 ou
+                        {                                            //Si l'ID existe déjà
+                            continue;                                  //On ne fait rien
+
+                        }
+
+                    }
+
+                    // Si le count == 1 alors on crée un nouveau compte
+
+                    if (subs.Count() == 1)
+                    {
+                        Banque.Add(new Compte(d));
+                        continue;
+
+                    }
+
+                    //SI le count == 2 alors on vérifier que la deuxième entrée est parsable en double avant
+                    //de créer un nouveau compte
+
+
+                    if (subs.Count() == 2)
+                    {
+                        if (double.TryParse(subs[1], out double e))
+                        {
+                            Banque.Add(new Compte(d, e));
+                           // Console.WriteLine("on passe par count == 2");
+
+                        }
+                        continue;
+
+                    }
+
+
+
+
+
+
+                }
+
+
+
+                file.Close();
+            }
+
+
+            foreach (Compte cp in Banque) 
+            {
+                cp.afficher();
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
